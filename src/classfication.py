b6df755fef9bed307a527473ca8e3fdf784f463a
win10 <doss72180@gmail.com>
2025-01-13 19:29:10 +0530

    argparse

--- src/classfication.py
+++ src/classfication.py
@@ -1,75 +1,83 @@
-# this is an example from the torhcHD library 
+# this is the example code from the original torchhd repository
+import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 from torchvision.datasets import MNIST
-
-# Note: this example requires the torchmetrics library: https://torchmetrics.readthedocs.io
 import torchmetrics
 from tqdm import tqdm
-
 import torchhd
 from torchhd.models import Centroid
 from torchhd import embeddings
 
+# Add argument parsing
+def parse_args():
+    parser = argparse.ArgumentParser(description='HD Computing MNIST Classification')
+    parser.add_argument('--dimensions', type=int, default=10000,
+                        help='number of dimensions (default: 10000)')
+    parser.add_argument('--img-size', type=int, default=28,
+                        help='input image size (default: 28)')
+    parser.add_argument('--num-levels', type=int, default=1000,
+                        help='number of levels (default: 1000)')
+    parser.add_argument('--batch-size', type=int, default=16,
+                        help='batch size (default: 16)')
+    parser.add_argument('--data-dir', type=str, default="../data",
+                        help='data directory (default: ../data)')
+    return parser.parse_args()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using {} device".format(device))
+def main():
+    args = parse_args()
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using {} device".format(device))
 
-DIMENSIONS = 10000
-IMG_SIZE = 28
-NUM_LEVELS = 1000
-BATCH_SIZE = 16  # for GPUs with enough memory we can process multiple images at ones
+    transform = torchvision.transforms.ToTensor()
 
-transform = torchvision.transforms.ToTensor()
+    train_ds = MNIST(args.data_dir, train=True, transform=transform, download=True)
+    train_ld = torch.utils.data.DataLoader(train_ds, batch_size=args.batch_size, shuffle=True)
 
-train_ds = MNIST("../data", train=True, transform=transform, download=True)
-train_ld = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+    test_ds = MNIST(args.data_dir, train=False, transform=transform, download=True)
+    test_ld = torch.utils.data.DataLoader(test_ds, batch_size=args.batch_size, shuffle=False)
 
-test_ds = MNIST("../data", train=False, transform=transform, download=True)
-test_ld = torch.utils.data.DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
+    class Encoder(nn.Module):
+        def __init__(self, out_features, size, levels):
+            super(Encoder, self).__init__()
+            self.flatten = torch.nn.Flatten()
+            self.position = embeddings.Random(size * size, out_features)
+            self.value = embeddings.Level(levels, out_features)
 
+        def forward(self, x):
+            x = self.flatten(x)
+            sample_hv = torchhd.bind(self.position.weight, self.value(x))
+            sample_hv = torchhd.multiset(sample_hv)
+            return torchhd.hard_quantize(sample_hv)
 
-class Encoder(nn.Module):
-    def __init__(self, out_features, size, levels):
-        super(Encoder, self).__init__()
-        self.flatten = torch.nn.Flatten()
-        self.position = embeddings.Random(size * size, out_features)
-        self.value = embeddings.Level(levels, out_features)
+    encode = Encoder(args.dimensions, args.img_size, args.num_levels)
+    encode = encode.to(device)
 
-    def forward(self, x):
-        x = self.flatten(x)
-        sample_hv = torchhd.bind(self.position.weight, self.value(x))
-        sample_hv = torchhd.multiset(sample_hv)
-        return torchhd.hard_quantize(sample_hv)
+    num_classes = len(train_ds.classes)
+    model = Centroid(args.dimensions, num_classes)
+    model = model.to(device)
 
+    with torch.no_grad():
+        for samples, labels in tqdm(train_ld, desc="Training"):
+            samples = samples.to(device)
+            labels = labels.to(device)
+            samples_hv = encode(samples)
+            model.add(samples_hv, labels)
 
-encode = Encoder(DIMENSIONS, IMG_SIZE, NUM_LEVELS)
-encode = encode.to(device)
+    accuracy = torchmetrics.Accuracy("multiclass", num_classes=num_classes)
 
-num_classes = len(train_ds.classes)
-model = Centroid(DIMENSIONS, num_classes)
-model = model.to(device)
+    with torch.no_grad():
+        model.normalize()
+        for samples, labels in tqdm(test_ld, desc="Testing"):
+            samples = samples.to(device)
+            samples_hv = encode(samples)
+            outputs = model(samples_hv, dot=True)
+            accuracy.update(outputs.cpu(), labels)
 
-with torch.no_grad():
-    for samples, labels in tqdm(train_ld, desc="Training"):
-        samples = samples.to(device)
-        labels = labels.to(device)
+    print(f"Testing accuracy of {(accuracy.compute().item() * 100):.3f}%")
 
-        samples_hv = encode(samples)
-        model.add(samples_hv, labels)
-
-accuracy = torchmetrics.Accuracy("multiclass", num_classes=num_classes)
-
-with torch.no_grad():
-    model.normalize()
-
-    for samples, labels in tqdm(test_ld, desc="Testing"):
-        samples = samples.to(device)
-
-        samples_hv = encode(samples)
-        outputs = model(samples_hv, dot=True)
-        accuracy.update(outputs.cpu(), labels)
-
-print(f"Testing accuracy of {(accuracy.compute().item() * 100):.3f}%")
+if __name__ == "__main__":
+    main()
